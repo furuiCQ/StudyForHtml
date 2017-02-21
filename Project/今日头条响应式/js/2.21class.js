fda6ad426835323c753ded4398e49b0c43e85114
furuiCQ <1182590636@qq.com>
2017-02-21 17:27:29 +0800

    实现ppt的效果

--- Project/今日头条响应式/js/2.21class.js
+++ Project/今日头条响应式/js/2.21class.js
@@ -3,7 +3,6 @@ $(function(){
 	$("input:eq(0)").click(function(e) {
 		
 		setInterval(function(){//设置计时器
-						console.log($(this));
 						var date=new Date();//获得当前时间date对象
 						$(".box").text(date);
 						},1000);
@@ -16,5 +15,6 @@ $(function(){
 						$(this).text("123");
 					
         });
+		$("div").val(333);
     });
 });
